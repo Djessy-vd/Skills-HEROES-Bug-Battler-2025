f46b2e5a2ce7672db46cf3b8df269da51819ef10
Djessy <168988972+Djessy-vd@users.noreply.github.com>
2026-09-23 14:21:26 +0200

    changed some things
    added the user questions an added som console logs and testjes tomorow im going back to finish some things and let it work

--- error-tool.js
+++ error-tool.js
@@ -1,6 +1,7 @@
 //require
 const readline = require('node:readline');
 const fs = require('fs');
+const { exit, kill, exitCode } = require('node:process');
 
 console.log('\x1b[33mde error tool is succesvol opgestart\x1b[0m');
 
@@ -14,25 +15,51 @@ function json (){
     const filePath = "./data.json";
     const content = '{}'
     if (fs.existsSync(filePath)) {
-        console.log('\x1b[32mdata File exists.\x1b[0m');    
+        console.log('\x1b[32mdata het bestand is er al en is goed\x1b[0m');    
     } else {
-        console.log('\x1b[31mdata File does not exist.\x1b[0m');
+        console.log('\x1b[31mdata bestand bestaad niet\x1b[0m');
         fs.writeFileSync(filePath, content);
-        console.log('\x1b[32mde log file is aangemaakt voor je\x1b[0m');
+        console.log('\x1b[32mde data bestand is aangemaakt voor je\x1b[0m');
     }
 };
 // functie uitvoen om te controleren of het data bestand bestaad
 console.log(json())
 
 function add_error (){
-    const rl = readline.createInterface({
+        const rl = readline.createInterface({
         input: process.stdin,
-        output: process.stdout,
+        output: process.stdout
     });
-        rl.question(`What's your name?`, name => {
-        console.log(`Hi ${name}!`);
+
+    // de vragen voor de gebruiker
+    rl.question('error-title: ', (title) => {
+    console.log(`de error title is ${title}!`);
+
+    // de priority vraag
+    rl.question('error priority (low / medium / high) ', (priority) => {
+        if (priority === 'low' || priority === 'medium' || priority === 'high') {
+            console.log(`de priority is ${priority}`);
+        }   else {
+            rl.close
+            console.log("er is een foutive antwoord gegeven probeer het opniew")
+            exit
+            add_error();
+        }
+        
+        
+        
+        // het interface sluiten
         rl.close();
+        console.log(title, priority)
     });
+    });
+
+    // ff testje
+    rl.on('close', () => {
+        console.log('Interface closed. Goodbye!');
+    });
+
+
 }
 
 add_error();
